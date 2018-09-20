fbf55df7a62950e86c3efc55cf9107a1f8d3f81b
yangmingzi7 <514273118@qq.com>
2018-09-20 09:50:03 +0800

    Update test_hello.py

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -14,3 +14,4 @@ class TestHello(unittest.TestCase):
     def test_world_unicode(self):
         """sample test with unicode"""
         self.assertEqual(hello(u'world'), u'hello world')
+        print('111')
